580e3a68f23052a6dc42dd8abef891f6a0b16e81
SebastianEPH <sebastianeph99@gmail.com>
2022-04-10 02:58:48 -0500

    was fixed knowledge front

--- src/componet/project/ExtraKnowledge.js
+++ src/componet/project/ExtraKnowledge.js
@@ -1,33 +1,30 @@
-import {Container} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
+import {LoadingDada} from "./loadingDada";
 
 export const ExtraKnowledge = ({data}) =>{
     const {extra_knowledge } = data
     return(
-        <Container>
-            {extra_knowledge &&
-                <>
-                    <h3 className={"title-h1 text-center mt-5 mb-3"}>Otros conocimientos del área de informática</h3>
-                    <div className="mb-4 mt-5 ">
-                        <div className="row d-grid gap-2 d-md-flex justify-content-center ">
-                            {extra_knowledge.map(({knowlegge,icon, dificulty}, index)=>{
-                                return(
-                                    <div key={"knowledge_"+ index} className="col-2 mb-4 ">
-                                        <div className="count-data text-center">
-                                            <span className="m-0px font-w-600">{dificulty}</span>
-                                            <div className="count h2" >
-                                                <img src={icon} width={"85px"} alt={knowlegge}/>
-                                                {/*{icon}*/}
-                                            </div>
 
-                                            <h5 className="m-0px font-w-600">{knowlegge}</h5>
-                                        </div>
+        <Container>
+            <h3  className={"title-h1 text-center mt-5 mb-3"}>Herramientas y librerías de proyectos</h3>
+            <Row className={"text-center-loading-vertical"}>
+                {extra_knowledge?
+                    extra_knowledge.map(({icon,dificulty, knowlegge}, index)=>
+                        <Col sm={3} key={"programming_language_"+ index} className={"mb-3 mt-2"}>
+                            <Row>
+                                <div className=" text-center">
+                                    <span className="m-0px font-w-600 text-white ">{dificulty}</span>
+                                    <div className="mb-2" >
+                                        <img src={icon} width={"85px"} alt={knowlegge}/>
                                     </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </>
-            }
+                                    <h5 className="m-0px font-w-600">{knowlegge}</h5>
+                                </div>
+                            </Row>
+                        </Col>
+                    )
+                    : <LoadingDada/>
+                }
+            </Row>
         </Container>
     )
 }
